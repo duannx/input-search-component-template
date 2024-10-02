a96b6e1407c3f8c1d946fecd4a2d489d1911c6b8
Nam Nguyen <nam.nguyen@electrolux.com>
2024-10-02 16:39:54 +0700

    Nam Nguyen - nam.nguyen

--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -1,18 +1,18 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
 
-import Input from '../components/Input';
+import Input from "../components/Input";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Input',
+  title: "Example/Input",
   component: Input,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   // args: { onClick: fn() },
 } satisfies Meta<typeof Input>;
@@ -24,6 +24,7 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     placeholder: "Type something to search...",
-    onSelectItem: fn()
+    debounceTime: 300,
+    onSelectItem: fn(),
   },
 };
