cf98a2174b59d8907e3c9a76db06ec63b59f8446
tien.nguyen2 <tien.nguyen2@niteco.se>
2024-10-05 21:01:47 +0700

    hi

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,6 +2,7 @@ import "./input.scss";
 import { fetchData } from "../../utils/fetch-data";
 import { debounce } from "../../utils/deboucne";
 import Loader from "../Loader";
+import { useEffect, useState } from "react";
 
 export interface InputProps {
   /** Placeholder of the input */
@@ -15,7 +16,71 @@ const Input = ({ placeholder, onSelectItem }: InputProps) => {
   console.log('input re-render')
 
   // Your code start here
-  return <input></input>
+
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>();
+  const [results, setResults] = useState<string[]>();
+  const [errorMsg, setErrorMsg] = useState<string>('');
+ 
+  const handleInputChanged = debounce((event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  }, 100);
+
+  useEffect(() => {
+    if (!inputValue) return;
+
+    let ignore = false;
+
+    setLoading(true);
+
+    fetchData(inputValue).then(res => {
+      if (ignore) return;
+      setResults(res)
+    }).catch(err => {
+      if (ignore) return;
+      setErrorMsg(err);
+    }).finally(() => {
+      if (ignore) return;
+      setLoading(false);
+    })
+
+    return () => {
+      // clean up function
+      ignore = true;
+    }
+  }, [inputValue])
+
+  const renderResults = () => {
+    if(!inputValue)
+      return null;
+    
+    if (isLoading) {
+      return <Loader />
+    }
+
+    if (errorMsg) {
+      return <div>{errorMsg}</div>
+    }
+
+    if (!results?.length) {
+      return <p> No results </p>
+    }
+
+    return <div>
+      {results.map(((result: string) => <div key={result} onClick={() => onSelectItem(result)}>{result}</div>
+      ))}
+    </div>
+  }
+
+  return (
+    <div>
+      <input
+      className="input"
+      placeholder={placeholder}
+      onChange={handleInputChanged}/>
+      {renderResults()}
+    </div>
+  )
   // Your code end here
 };
 
