1992443b4ef8a05888d890751c2a137fb53a6a59
Nam Le <nam.le@electrolux.com>
2024-10-05 08:06:13 +0700

    add input search component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,6 +2,7 @@ import "./input.scss";
 import { fetchData } from "../../utils/fetch-data";
 import { debounce } from "../../utils/deboucne";
 import Loader from "../Loader";
+import { useRef, useState } from "react";
 
 export interface InputProps {
   /** Placeholder of the input */
@@ -9,13 +10,63 @@ export interface InputProps {
   /** On click item handler */
   onSelectItem: (item: string) => void;
 }
-
+export enum InputState {
+  INITITAL = 'initital',
+  FETCHING = 'fetching',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
 const Input = ({ placeholder, onSelectItem }: InputProps) => {
   // DO NOT remove this log
   console.log('input re-render')
 
   // Your code start here
-  return <input></input>
+  const [status, setStatus] = useState<InputState>(InputState.INITITAL);
+  const [data, setData] = useState<string[]>();
+  const [error, setError] = useState<string>('');
+
+  const lastTimeID = useRef<number>(0);
+
+  const searchText = async (query: string) => {
+    if(!query) {
+      setStatus(InputState.INITITAL);
+      setData([]);
+      setError('');
+      lastTimeID.current = 0;
+      return;
+    }
+    const currentTime = new Date().getTime();
+    lastTimeID.current = currentTime;
+    try {
+      if(status !== InputState.FETCHING) {
+        setStatus(InputState.FETCHING);
+      }
+      const response = await fetchData(query);
+
+      if(lastTimeID.current !== currentTime) return;
+
+      setData(response);
+      setStatus(InputState.SUCCESS);
+
+    } catch (error) {
+      if(lastTimeID.current !== currentTime) return;
+
+      setStatus(InputState.ERROR);
+      setError(error as string)
+    }
+  }
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => debounce(searchText(event.target.value), 100)
+
+  return <div className="search-container">
+    <input className="search-container__input" onChange={handleChange} placeholder={placeholder}/>
+    {status !== InputState.INITITAL && <div className="search-container__content">
+        {status === InputState.FETCHING && <Loader/>}
+        {status === InputState.ERROR && <div className="search-container__error">{error}</div>}
+        {status === InputState.SUCCESS && <div className="search-container__result">
+          {!!data?.length ? data.map((item, index) => <div key={index} onClick={() => onSelectItem(item)}>{item}</div>) : 'No results'}
+        </div>}
+    </div>}
+  </div>
   // Your code end here
 };
 
