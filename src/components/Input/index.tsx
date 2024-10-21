051860fe1997983f4125956164227f616fed42a7
NITECO\nha.le <nha.le@niteco.se>
2024-10-21 19:07:59 +0700

    update todos

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,92 +1,137 @@
 import "./input.scss";
-import { fetchData } from "../../utils/fetch-data";
-import { debounce } from "../../utils/deboucne";
-import Loader from "../Loader";
-import { ChangeEvent, useEffect,  useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export interface InputProps {
-  /** Placeholder of the input */
-  placeholder?: string;
-  /** On click item handler */
-  onSelectItem: (item: string) => void;
+interface ITodo {
+  title: string,
+  isEditing: boolean,
+  status: boolean
 }
 
-const DEBOUNCE_TIME = 100; //ms
-
-const Input = ({ placeholder, onSelectItem }: InputProps) => {
-  const [searchText, setSearchText] = useState('');
-  const [items, setItems] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  // DO NOT remove this log
-  console.log("input re-render");
+const Input = () => {
+  const [todoList, setTodoList] = useState<ITodo[]>([]);
+  const [filterStatus, setFilterStatus] = useState<string>();
+  const inputRef = useRef(null);
 
   useEffect(() => {
-    if(!searchText) {
-      return ;
-    }
-    let ignore = false;
-    setIsLoading(true);
-    setErrorMessage("");
-    fetchData(searchText)
-      .then((response) => {
-        if (!ignore) {
-          setItems(response || []);
-        }
-      })
-      .catch((error) => {
-        if (!ignore) {
-          setErrorMessage(error);
-        }
-      })
-      .finally(() => {
-        if (!ignore) {
-          setIsLoading(false);
-        }
-      });
-    return () => {
-      ignore = true;
-    }
-  }, [searchText])
+    // document.addEventListener('click', () => {
+    //   setTodoList(prevList => prevList.map((item) => {
+    //     item.isEditing = false
+    //     return item
+    //   }))
+    // })
+  }, [])
 
-  const onChangeInput = debounce((event: ChangeEvent) => {
-    setSearchText((event.target as HTMLInputElement).value)
-  }, DEBOUNCE_TIME);
+  const toggleStatusTodoItem = (itemIndex: number) => {
+    setTodoList(prevList => prevList.map((item, index) => {
+      if(index === itemIndex) {
+        item.status = !item.status
+      }
+      return item
+    }))
+  }
 
-  const renderSearchResult = () => {
-    if (isLoading) {
-      return <Loader />;
+  const addNewItem = (event) => {
+    if (event.key === 'Enter' && event.target.value) {
+      setTodoList(prevList => [...prevList, {
+        title: event.target.value,
+        isEditing: false,
+        status: false
+      }])
+      setTimeout(() => {
+        inputRef.current.value = '';
+      })
     }
-    if (errorMessage) {
-      return (
-        <div className={"search__error-message"}>{errorMessage}</div>
-      );
+  }
+
+  const updateItem = (itemIndex: number, itemKey, itemValue) => {
+    setTodoList(prevList => prevList.map((item, index) => {
+        if(index === itemIndex) {
+          item[itemKey] = itemValue
+        }
+        return item
+      })
+    )
+  }
+
+  const changeTitleTodoItem = (itemIndex, event) => {
+    if (event.key === 'Enter' && event.target.value) {
+      updateItem(itemIndex, 'title', event.target.value)
+      updateItem(itemIndex, 'isEditing', false)
     }
-    if (items.length === 0) {
-      return <div className={'search__no-result'}>No result</div>
+  }
+
+  const onClickItem = (event) => {
+    event.stopPropagation()
+  }
+
+  const removeItem = (itemIndex: number) => {
+    setTodoList(prevList => prevList.filter((item, index) => index != itemIndex))
+  }
+
+  const clearCompletedItem = () => {
+    setTodoList(prevList => prevList.filter((item) => !item.status))
+  }
+
+  const renderTodoList = () => {
+    if (todoList.length === 0) {
+      return null
     }
+
     return (
-      <ul className={'search__list'}>
-        {items.map((item, index) => (
-          <li className={'search__item'} key={item + index} onClick={() => onSelectItem(item)}>{item}</li>
-        ))}
-      </ul>
-    );
-  };
-
-  // Your code start here
-  return (
-    <div className={"search"}>
-      <input
-        className={"search__input"}
-        placeholder={placeholder}
-        onChange={onChangeInput}
-      />
-      {searchText && (
-        <div className={"search__result"}>
-          {renderSearchResult()}
+      <div className={"todo-list"}>
+        {
+          todoList.filter(item => {
+            if(!filterStatus) {
+              return true
+            }
+            return filterStatus === 'completed' ? item.status : !item.status
+          }).map((todoItem, index) => <div className={"todo-item"} key={index} onClick={onClickItem}>
+            {
+              todoItem.isEditing ?
+                <input type="text" defaultValue={todoItem.title}
+                       autoFocus
+                       onKeyDown={(event) => changeTitleTodoItem(index, event)} onBlur={() => updateItem(index, 'isEditing', false)} /> :
+                <div className={`todo-item__wrapper ${todoItem.status ? 'finished' : ""}`}>
+                  <button type="checkbox" className={`btn-checkbox `} onClick={() => updateItem(index, 'status', !todoItem.status)} >
+                    <span className={'check-icon'}></span>
+                  </button>
+                  <div className={"todo-item__title"} onDoubleClick={() => updateItem(index, 'isEditing', true)}
+                       title={"Double click to edit title"}>
+                    {todoItem.title}
+                  </div>
+                  <button className={'btn-close'} title={"remove todo item"} onClick={() => removeItem(index)}>
+                    X
+                  </button>
+                </div>
+            }
+          </div>)
+        }
+        <div className={"todo__tool-bar"}>
+          <span>{todoList.filter(i => !i.status).length} items left!</span>
+          <div className={"todo__filter"}>
+            <button onClick={() => setFilterStatus('')} className={filterStatus ? '' : 'active'}>All</button>
+            <button onClick={() => setFilterStatus('active')} className={filterStatus === 'active' ? 'active' : ''}>Active</button>
+            <button onClick={() => setFilterStatus('completed')} className={filterStatus === 'completed' ? 'active' : ''}>Completed</button>
+          </div>
+          <button onClick={clearCompletedItem}>Clear Completed</button>
         </div>
-      )}
+      </div>
+    )
+  }
+
+  return (
+    <div className={"todo"}>
+      <h1>todos</h1>
+      <input
+        className={"todo__input"}
+        placeholder={"What needs to be done?"}
+        onKeyDown={addNewItem}
+        ref={inputRef}
+      />
+      {renderTodoList()}
+      <div className={"todo__desc"}>
+        Double-click to edit a todo
+      </div>
     </div>
   );
   // Your code end here
