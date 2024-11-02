2f2879eace0aeb97fa15d51d566fdcd04c077d71
Anh Vu <sara.vu@vmodev.com>
2024-11-02 13:13:00 +0700

    vu.nguyen4

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,6 +2,7 @@ import "./input.scss";
 import { fetchData } from "../../utils/fetch-data";
 import { debounce } from "../../utils/deboucne";
 import Loader from "../Loader";
+import { ChangeEvent, useCallback, useState } from "react";
 
 export interface InputProps {
   /** Placeholder of the input */
@@ -14,8 +15,59 @@ const Input = ({ placeholder, onSelectItem }: InputProps) => {
   // DO NOT remove this log
   console.log('input re-render')
 
+  const [result, setResult] = useState<string[]>([]);
+
+  const [text, setText] = useState<string>('')
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onFetch = debounce(async (input: string) => {
+    try {
+      if (!input) {
+        setResult([]);
+        return;
+      }
+      setIsLoading(true);
+      const response = await fetchData(input);
+      setResult(response);
+    } catch (err) {
+      console.log("Err", err);
+    } finally {
+      setIsLoading(false);
+    }
+  }, 1000)
+
+  const handleFetchData = useCallback(async (event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+    await onFetch(event.target.value);
+  }, [])
+
+  const handleSelectItem = useCallback((item: string) => {
+    setText(item);
+    onSelectItem(item)
+  }, [])
+
   // Your code start here
-  return <input></input>
+  return (
+    <div>
+      <input value={text} onChange={handleFetchData} placeholder={placeholder}></input>
+      {
+        isLoading && (
+          <Loader />
+        )
+      }
+      {
+        result &&
+        result.map((item, index) => {
+          return (
+            <div key={index} onClick={() => handleSelectItem(item)}>
+              {item}
+            </div>
+          )
+        })
+      }
+    </div>
+  )
   // Your code end here
 };
 
